73fa511f56aed142b4f29309b5912e5853178a65
revanth102938 <revanth.ace66@gmail.com>
2025-06-25 00:22:16 +0530

    configured frontend to connect with backend

--- auctionfront/src/utils/ApiConfig.js
+++ auctionfront/src/utils/ApiConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true, 
 });
 
