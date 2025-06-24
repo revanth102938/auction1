800506e7ab1ad0c25a590aa1ed4e636c41d3b8d3
revanth102938 <revanth.ace66@gmail.com>
2025-06-25 00:29:03 +0530

    minor change

--- auctionback/src/server.js
+++ auctionback/src/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN,
     methods: ['GET', 'POST'],
     credentials: true,
   },
